c971e61e75831df8fe0dac1ac7ade4a438a109a8
yunhua.xiao <yunhua.xiao@dianping.com>
2013-08-23 16:30:56 +0800

    proj init

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+    // 以下代码初始化Grunt任务
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        stylus: {
+            compile: {
+                options: {
+                    paths: ['public/stylesheets'],
+                    // urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+                    use: [
+                        //require('fluidity') // use stylus plugin at compile time
+                    ],
+                    import: [] //  @import 'foo', 'bar/moo', etc. into every .styl file
+                },
+                files: {
+                    'public/css/app.css': ['public/stylus/*.styl'] // compile and concat into single file
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: ['**/*.stylus'],
+                tasks: ['stylus'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+        },
+
+        copy: {
+            main: {
+                files: [
+                    { expand: true, cwd: 'public/stylus/img/', src: ['**'], dest: 'public/css/img/' }
+                ]
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-stylus')
+    grunt.loadNpmTasks('grunt-contrib-copy')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    
+    grunt.registerTask('dev', ['stylus', 'copy', 'watch'])
+};
---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+(function (WIN, UNDF) {
+	var modA = {
+		init: function () {
+
+		}
+	}
+	// 前端 javascript 入口
+	$(function () {
+		modA.init()
+	})
+
+})(window)
