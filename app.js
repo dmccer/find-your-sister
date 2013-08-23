7b6b8fc4a15aef9a4a1c937c1b9c17552a5ec727
yuchen.yang <yuchen.yang@dianping.com>
2013-08-24 02:43:24 +0800

    search

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
- * Module dependencies.
+ /* Module dependencies.
  */
 
 
 var express = require('express')
 var routes = require('./routes')
-var person = require('./routes/person')
 var location = require('./routes/location')
-var user = require('./routes/user')
 var dper = require('./routes/dper')
 var card = require('./routes/card')
 var seat = require('./routes/seat')
@@ -42,7 +40,6 @@ app.get('/card/name/:name', card.getByName)
 app.get('/card/dept/:dept', card.getByDept)
 
 app.get('/seat/:name', seat.getByName)
-app.get('/person', person.info)
 app.get('/location', location.info)
 app.get('/meeting/room/:email', meeting.getByEmail)
 app.get('/meeting/dper/:dper', meeting.getByDper)
